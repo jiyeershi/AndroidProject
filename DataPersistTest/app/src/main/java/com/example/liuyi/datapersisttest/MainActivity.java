0d6dab461fe4ed6c9902bddaa1890fd954bbccb0
liuyi <liuyi-124772115@163.com>
2017-11-12 14:09:50 +0800

    add DataPersistTest/

---
+++ DataPersistTest/app/src/main/java/com/example/liuyi/datapersisttest/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.liuyi.datapersisttest;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.eidtText);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        String cacheStr = load();
+        //字符串的判断可以用TextUtils.isEmpty(cacheStr),该函数可以判断空字符串和null
+        if (!TextUtils.isEmpty(cacheStr)) {
+            editText.setText(cacheStr);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        save();
+    }
+
+    private void save() {
+        FileOutputStream out = null;
+        BufferedWriter writer = null;
+        try {
+            out = openFileOutput("data", Context.MODE_PRIVATE);//覆盖
+//            out = openFileOutput("data", Context.MODE_APPEND);//累加形式
+            writer = new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(editText.getText().toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private  String load() {
+        FileInputStream in = null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+        try {
+            in = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return content.toString();
+    }
+}
